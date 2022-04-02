8e9188948842cb9672e79f74cbbfd4a69e727fc4
Csaba Gyuris <gyuris.csaba@vasvari.org>
2022-04-02 10:00:22 +0200

    Projekt fájl módosítások

--- BooksProjekt/Model/Book.cs
+++ BooksProjekt/Model/Book.cs
@@ -4,37 +4,7 @@ using System.Text;
 
 namespace Konyvek.model
 {
-    public abstract class Book
+    class Book
     {
-        /// <summary>
-        /// könyv címe
-        /// </summary>
-        protected string title;
-        /// <summary>
-        /// könyv oldalszáma
-        /// </summary>
-        protected int pageNumber;
-        /// <summary>
-        /// könyv szerzője
-        /// </summary>
-        protected string author;
-        /// <summary>
-        /// konstruktor
-        /// </summary>
-        /// <param name="title">könyv címe</param>
-        /// <param name="pageNumber">könyv oldalszáma</param>
-        /// <param name="author">könyv szerzője</param>
-        protected Book(string author, string title, int pageNumber)
-        {
-            this.title = title;
-            this.pageNumber = pageNumber;
-            this.author = author;
-        }
-
-        public override string ToString()
-        {
-            return "Könyv szerzője: " + author + "\nKönyv címe: " + title + "\nKönyv oldalszáma: " + pageNumber;
-        }
-
     }
 }
